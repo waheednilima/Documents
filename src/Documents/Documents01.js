ce624d480f4ec7dbfc237a8923e704468e05e13e
MOHAMMAD MAHMUD HOSAIN <mahmud.hosain.pu@gmail.com>
2023-07-08 19:38:38 +0600

    update

--- src/Documents/Documents01.js
+++ src/Documents/Documents01.js
@@ -4,12 +4,25 @@ const Documents01 = ({ onScroll }) => {
 
   return (
     <div className="">
+      <div className="container w-full h-screen mx-auto">
 
-      <div className="container mx-auto py-20 ">
-        <h1>Moinul Hosain</h1>
+
+          <div className="flex flex-col justify-center space-y-1 my-auto py-20">
+
+             <h1 className='text-center text-4xl font-bold'>মজুমদার ট্রাভেল</h1>
+             <h2 className='text-center font-bold text-color7'>ব্যবসার/পেশার ধরণ:ট্রাভেল এজেন্সি</h2>
+             <h2 className='text-center font-bold text-color7'>স্বত্ত্বাধীকারী/লাইসেন্সধারী: শামীম মজুমদার</h2>
+             <h2 className='text-center font-bold text-color7'>পিতা: এম কে আলম মজুমদার</h2>
+             <h2 className='text-center font-bold text-color7'>মাতা: নিলুফা বেগম</h2>
+             <h2 className='text-center font-bold text-color7'>ট্রেড লাইসেন্স নাম্বারঃ০৮০২১১১০০১২৩১১৫</h2>
+             <h2 className='text-center font-bold text-color3'>মেয়াদ: ৩০-০৬-২০২৪ খ্রিঃ</h2>
+
+
+          </div>
+
+
+
       </div>
-
-
     </div>
   )
 }
